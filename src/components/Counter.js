528b712283715b26912ac3c1c8fe29f52ec315cb
Brittany Halterman <halterma@gmail.com>
2025-03-04 18:28:10 -0500

    Lift state up

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,29 +1,27 @@
 import React from 'react';
 
-const Counter = () => {
-  const [score, setScore] = React.useState(0);
+const Counter = (props) => {
+  // const incrementScore = () => {
+  //   setScore((prevScore) => prevScore + 1);
+  // };
 
-  const incrementScore = () => {
-    setScore((prevScore) => prevScore + 1);
-  };
-
-  const decrementScore = () => {
-    setScore((prevScore) => prevScore - 1);
-  };
+  // const decrementScore = () => {
+  //   setScore((prevScore) => prevScore - 1);
+  // };
 
   return (
     <div className="counter">
       <button
         className="counter-action decrement"
-        onClick={() => decrementScore()}
+        // onClick={() => decrementScore()}
       >
         {' '}
         -{' '}
       </button>
-      <span className="counter-score">{score}</span>
+      <span className="counter-score">{props.score}</span>
       <button
         className="counter-action increment"
-        onClick={() => incrementScore()}
+        // onClick={() => incrementScore()}
       >
         {' '}
         +{' '}
