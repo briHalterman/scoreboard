97ffcb8dbd690bc139349224585aa3e9b03a9bce
Brittany Halterman <halterma@gmail.com>
2025-03-06 13:16:33 -0500

    Use useRef hook to access and interact with DOM nodes

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 
 const AddPlayerForm = ({ addPlayer }) => {
-  const [value, setValue] = useState('');
+  const playerInput = useRef();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    addPlayer(value);
-    setValue('');
+    addPlayer(playerInput.current.value);
+    event.currentTarget.reset();
   };
 
   return (
     <form onSubmit={(event) => handleSubmit(event)}>
+      {console.log(playerInput)}
       <input
         type="text"
-        value={value}
+        ref={playerInput}
         placeholder="Enter a player's name"
-        onChange={(event) => setValue(event.target.value)}
       />
       <input type="submit" value="Add player" />
     </form>
