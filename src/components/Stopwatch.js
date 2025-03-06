80211b450d0be7639bd95b1e6ef1eee8b0199f4b
Brittany Halterman <halterma@gmail.com>
2025-03-06 09:33:34 -0500

    Implement stopwatch state

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 
 const Stopwatch = () => {
+  const [isRunning, setIsRunning] = useState(false);
   return (
-    <div className='stopwatch'>
+    <div className="stopwatch">
       <h2> Stopwatch</h2>
-      <span className='stopwatch-time'>0</span>
-      <button>Start</button>
+      <span className="stopwatch-time">0</span>
+      <button onClick={() => setIsRunning((prevValue) => !prevValue)}>
+        {isRunning ? 'Stop' : 'Start'}
+      </button>
       <button>Reset</button>
     </div>
   );
-}
+};
 
 export default Stopwatch;
