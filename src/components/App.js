e684af9b23036d7c7eb2bc41b419e5087d316433
Brittany Halterman <halterma@gmail.com>
2025-03-14 10:03:07 -0400

    Implement PlayerList.js to dynamically render players using Context API

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,14 @@
 import React from 'react';
 import Header from './Header';
-import Player from './Player';
+import PlayerList from './PlayerList';
 import AddPlayerForm from './AddPlayerForm';
 
 const App = () => {
-  const [players, setPlayers] = useState([
-    {
-      name: 'Guil',
-      score: 0,
-      id: 1,
-    },
-    {
-      name: 'Treasure',
-      score: 0,
-      id: 2,
-    },
-    {
-      name: 'Ashley',
-      score: 0,
-      id: 3,
-    },
-    {
-      name: 'James',
-      score: 0,
-      id: 4,
-    },
-  ]);
-
-  const [highScore, setHighScore] = useState();
-  const nextPlayerId = useRef(5);
-
-  useEffect(() => {
-    const scores = players.map((player) => player.score);
-    setHighScore(Math.max(...scores));
-  }, [players]);
-
   return (
     <div className="scoreboard">
-      <Header players={players} />
-
-      {/* Players list */}
-      {players.map((player) => (
-        <Player
-          name={player.name}
-          score={player.score}
-          id={player.id}
-          key={player.id.toString()}
-          removePlayer={handleRemovePlayer}
-          changeScore={handleScoreChange}
-          isHighScore={player.score === highScore && highScore !== 0}
-        />
-      ))}
-      <AddPlayerForm addPlayer={handleAddPlayer} />
+      <Header title="Scoreboard" />
+      <PlayerList />
+      <AddPlayerForm />
     </div>
   );
 };
