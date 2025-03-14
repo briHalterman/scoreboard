db6d47d67d4765ef92ecbb64999af073c977fa1d
Brittany Halterman <halterma@gmail.com>
2025-03-14 09:17:20 -0400

    Wrap App in Context Provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { Provider } from './Components/Context';
 import App from './Components/App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
